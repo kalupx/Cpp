7754cb0db046a463a5b9a3caab6919a151bbcc18
jefte gomes <jefte.paula@ufv.br>
2022-02-04 13:19:12 -0300

    pratica1.2 pronta

--- pratica 1/pratica1.2.cpp
+++ pratica 1/pratica1.2.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+void leiaInteiros1( int &a , int &b) {
+    cin >> a >> b;
+}
+
+void leiaInteiros2( int *a , int *b) {
+    cin >> *a >> *b;
+}
+
+int main () {
+    int a , b ;
+    // passagem por referencia
+    leiaInteiros1 (a, b) ;
+    cout << a << " e " << b << endl;
+    
+    // utilize ponteiros
+    leiaInteiros2 (&a, &b) ;
+    cout << a << " e " << b << endl;
 
 
-
-
-
-
-
-
-
-
-    return 0;
+return 0;
 }
