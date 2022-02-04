39807755daca8f1ced779113f6e346304c2d8cec
jefte gomes <jefte.paula@ufv.br>
2022-02-04 13:38:37 -0300

    primeira funcao pronta

--- pratica 1/pratica1.3.cpp
+++ pratica 1/pratica1.3.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
-
 using namespace std;
 
-int main(){
 
+int calculaTamanhoString1 ( char *str) {
+    int ctr = 0;
+    for (int i = 0; *(str+i) != '\0'; i++)
+    {
+        ctr++;
+    }
+    return ctr;
+}
+/*
+int calculaTamanhoString2 ( char ...) {
+// insira seu codigo aqui ...
+}
 
-
-    return 0;
+int calculaTamanhoString3 ( char ...) {
+// insira seu codigo aqui ...
+}
+*/
+int main () {
+char str [51];
+cout << " Digite alguma string ... ( com ate 50 caracteres ):";
+cin . getline ( str , 50) ;
+cout << " Tamanhos calculados :" << endl ;
+cout << calculaTamanhoString1 ( str ) << " ";// << calculaTamanhoString2 ( str ) << " " << calculaTamanhoString3 ( str ) ;
+cout << endl ;
+return 0;
 }
