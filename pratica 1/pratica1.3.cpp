e832b9551f5b1aa8a74b0eaa4351e27d3eec22e8
jefte gomes <jefte.paula@ufv.br>
2022-02-04 13:49:49 -0300

    funcao 2 pronta

--- pratica 1/pratica1.3.cpp
+++ pratica 1/pratica1.3.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 
-int calculaTamanhoString1 ( char *str) {
+int calculaTamanhoString1 ( char str[]) {
+    int ctr = 0;
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        ctr++;
+    }
+    
+    return ctr;
+}
+
+int calculaTamanhoString2 ( char *str) {
     int ctr = 0;
     for (int i = 0; *(str+i) != '\0'; i++)
     {
@@ -10,11 +20,8 @@ int calculaTamanhoString1 ( char *str) {
     }
     return ctr;
 }
-/*
-int calculaTamanhoString2 ( char ...) {
-// insira seu codigo aqui ...
-}
 
+/*
 int calculaTamanhoString3 ( char ...) {
 // insira seu codigo aqui ...
 }
@@ -24,7 +31,7 @@ char str [51];
 cout << " Digite alguma string ... ( com ate 50 caracteres ):";
 cin . getline ( str , 50) ;
 cout << " Tamanhos calculados :" << endl ;
-cout << calculaTamanhoString1 ( str ) << " ";// << calculaTamanhoString2 ( str ) << " " << calculaTamanhoString3 ( str ) ;
+cout << calculaTamanhoString1 ( str ) << " " << calculaTamanhoString2 ( str ) << " ";// << calculaTamanhoString3 ( str ) ;
 cout << endl ;
 return 0;
 }
