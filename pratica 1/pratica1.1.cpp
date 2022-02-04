9b3b6da744309f527dc0359c0f4d2134aa01a02d
jefte gomes <jefte.paula@ufv.br>
2022-02-04 13:03:55 -0300

    pratica1.1 finalizada

--- pratica 1/pratica1.1.cpp
+++ pratica 1/pratica1.1.cpp
@@ -1,9 +1,21 @@
-#includue <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
 
+    int long value2, value1 = 200000, *longptr;
 
+    longptr = &value1;
 
+    cout << *longptr << endl;
+    
+    value2 = *longptr;
+    cout << value2 << endl;
+
+    cout << &value2 << endl;
+
+    cout << &value1 << endl;
+    cout << longptr << endl;
+    //os valores sao iguais pois longptr armazena o endereco de 1;
     return 0;
 }
