e4fa5035fdcbec44fc94c28c13d6b67e6ef54a6d
jefte gomes <jefte.paula@ufv.br>
2022-02-04 13:54:07 -0300

    pratica1.3 pronta

--- pratica 1/pratica1.3.cpp
+++ pratica 1/pratica1.3.cpp
@@ -21,17 +21,23 @@ int calculaTamanhoString2 ( char *str) {
     return ctr;
 }
 
-/*
-int calculaTamanhoString3 ( char ...) {
-// insira seu codigo aqui ...
+int calculaTamanhoString3 ( char *str) {
+    int ctr = 0;
+    for (int i = 0; *str != '\0'; i++)
+    {
+        ctr++;
+        str++;
+    }
+    return ctr;
 }
-*/
+
 int main () {
 char str [51];
 cout << " Digite alguma string ... ( com ate 50 caracteres ):";
 cin . getline ( str , 50) ;
 cout << " Tamanhos calculados :" << endl ;
-cout << calculaTamanhoString1 ( str ) << " " << calculaTamanhoString2 ( str ) << " ";// << calculaTamanhoString3 ( str ) ;
+cout << calculaTamanhoString1 ( str ) << " " << calculaTamanhoString2 ( str ) << " " << calculaTamanhoString3 ( str ) ;
 cout << endl ;
+
 return 0;
 }
