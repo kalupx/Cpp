00597b4b1ec0142ae866bf22c5962cfeeca6d9ac
jefte gomes <jefte.paula@ufv.br>
2022-02-04 14:16:59 -0300

    implementando o for com structs

--- pratica 1/pratica1.4.cpp
+++ pratica 1/pratica1.4.cpp
@@ -5,13 +5,26 @@ struct Jogador {
     int pontos ;
     int x , y ;
 };
-
-void leDadosJogador ( Jogador ...) {
-// insira seu codigo aqui ...
+/*
+void leDadosJogador ( Jogador *jogador) {
+    cin >> j.x;
+    cin >> j.y;
 }
-
+*/
 int main () {
     Jogador j [5];
-// insira seu codigo aqui ...
+
+    
+    cin >> j[1].x;
+    cin >> j[1].y;
+    cout << "x: " << j[1].x << " y: " << j[1].y << endl;
+    /*
+    for (int i = 0; i < 5; i++)
+    {
+        leDadosJogador(j[i]);
+    }
+    */
+    
+
 return 0;
 }
