57f7a5c9642965e175b0ee12d8490db8c1a861a2
jefte gomes <jefte.paula@ufv.br>
2022-02-04 15:27:35 -0300

    pratica2.1

---
+++ Pratica_2_Jefte-Gomes_105469/pratica2.1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+void preencheAleatorios ( int ... , int ...) {
+// insira seu codigo aqui ...
+}
+
+void imprime ( int ... , int ...) {
+// insira seu codigo aqui ...
+}
+
+void contaParImpar ( int .. , int ... , int ... , int ... , int ...) {
+// insira seu codigo aqui ...
+}
+
+int main () {
+// insira seu codigo aqui ...
+
+
+return 0;
+}
