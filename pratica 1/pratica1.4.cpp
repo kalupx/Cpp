75528f86967d61fefda5f562465bc00df8d3c111
jefte gomes <jefte.paula@ufv.br>
2022-02-04 14:04:20 -0300

    pratica1.4

---
+++ pratica 1/pratica1.4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+struct Jogador {
+    int pontos ;
+    int x , y ;
+};
+
+void leDadosJogador ( Jogador ...) {
+// insira seu codigo aqui ...
+}
+
+int main () {
+    Jogador j [5];
+// insira seu codigo aqui ...
+return 0;
+}
