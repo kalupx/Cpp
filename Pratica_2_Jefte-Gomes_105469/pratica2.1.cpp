cad2e8b2e24309868daf91ee57ea2d3f7043d63f
jefte gomes <jefte.paula@ufv.br>
2022-02-04 16:45:08 -0300

    preenche e imprime pronto

--- Pratica_2_Jefte-Gomes_105469/pratica2.1.cpp
+++ Pratica_2_Jefte-Gomes_105469/pratica2.1.cpp
@@ -1,22 +1,47 @@
 #include <iostream>
+#include <ctime>
+
 using namespace std;
 
 
-void preencheAleatorios ( int ... , int ...) {
-// insira seu codigo aqui ...
+void preencheAleatorios ( int *ptr, int n) {
+    for (int i = 0; i < n; i++)
+    {
+        *(ptr+i) = rand()%10;
+    }
+    
 }
 
-void imprime ( int ... , int ...) {
-// insira seu codigo aqui ...
+
+void imprime ( int *ptr , int n) {
+    for (int i = 0; i < n; i++)
+    {
+        cout << *(ptr+i) << " ";
+    }
 }
 
+/*
 void contaParImpar ( int .. , int ... , int ... , int ... , int ...) {
 // insira seu codigo aqui ...
 }
+*/
+
 
 int main () {
 // insira seu codigo aqui ...
+    unsigned seed = time(0);
 
+    srand(seed);
+
+    int *a, *b, n;
+    cin >> n;
+    
+    a = (int *) malloc(sizeof(int)*n);
+    b = (int *) malloc(sizeof(int)*n);
+    
+    preencheAleatorios(a, n);
+    preencheAleatorios(b, n);
+    imprime(a, n);
 
 return 0;
 }
