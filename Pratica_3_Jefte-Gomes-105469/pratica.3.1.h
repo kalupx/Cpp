00f7ef69739a70b17309a08fbbc03150411c53a8
jefte gomes <jefte.paula@ufv.br>
2022-02-07 12:34:30 -0300

    funcoes implementadas

--- Pratica_3_Jefte-Gomes-105469/pratica.3.1.h
+++ Pratica_3_Jefte-Gomes-105469/pratica.3.1.h
@@ -16,5 +16,24 @@ struct ListaVetorInteiros{
     void imprimir();
 };
 
+void  ListaVetorInteiros::inserir_elemento(int elemento){
+    if(_num_elementos_inseridos == TAMANHO){
+        std::cerr << "Erro, lista cheia" << std::endl;
+        exit(1);
+    }
+    _elementos[_num_elementos_inseridos] = elemento;
+    _num_elementos_inseridos++;
+}
 
+void ListaVetorInteiros::imprimir(){
+    for (int i = 0; i < _num_elementos_inseridos; i++)
+    {
+        std::cout << _elementos[i] << " ";
+    }
+    
+}
+
+ListaVetorInteiros::~ListaVetorInteiros(){
+    delete[] _elementos;
+}
 #endif
