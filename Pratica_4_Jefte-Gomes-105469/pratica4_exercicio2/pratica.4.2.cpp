54a31f127f1c199cb195b68535ef93d184721632
jefte gomes <jefte.paula@ufv.br>
2022-02-16 11:39:02 -0300

    construtor pronto

---
+++ Pratica_4_Jefte-Gomes-105469/pratica4_exercicio2/pratica.4.2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "pratica.4.2.h"
+
+int main () {
+
+/*
+Aquecedor aq1 ;
+std :: cout << aq1 . getTemperatura () << std :: endl ;
+
+aq1 . aquecer () ;
+std :: cout << aq1 . getTemperatura () << std :: endl ;
+
+aq1 . esfriar () ;
+aq1 . esfriar () ;
+
+std :: cout << aq1 . getTemperatura () << std :: endl ;
+*/
+return 0;
+}
---
+++ Pratica_4_Jefte-Gomes-105469/pratica4_exercicio2/pratica.4.2.h
@@ -0,0 +1,12 @@
+#ifndef AQUECEDOR_H
+#define AQUECEDOR_H 
+ 
+class Aquecedor{
+    private:
+        float temperatura;
+    public:
+        Aquecedor();
+};
+Aquecedor::Aquecedor(): temperatura(20) {}
+
+#endif
