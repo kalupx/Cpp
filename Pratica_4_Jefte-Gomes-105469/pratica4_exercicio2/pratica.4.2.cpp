d4b6045fccfa9a8bea273805f75ac32fd3323e56
jefte gomes <jefte.paula@ufv.br>
2022-02-16 11:54:24 -0300

    4.2 pronta

--- Pratica_4_Jefte-Gomes-105469/pratica4_exercicio2/pratica.4.2.cpp
+++ Pratica_4_Jefte-Gomes-105469/pratica4_exercicio2/pratica.4.2.cpp
@@ -3,9 +3,10 @@
 
 int main () {
 
-/*
+
 Aquecedor aq1 ;
-std :: cout << aq1 . getTemperatura () << std :: endl ;
+
+std :: cout << aq1 . getTemperatura() << std :: endl ;
 
 aq1 . aquecer () ;
 std :: cout << aq1 . getTemperatura () << std :: endl ;
@@ -14,6 +15,6 @@ aq1 . esfriar () ;
 aq1 . esfriar () ;
 
 std :: cout << aq1 . getTemperatura () << std :: endl ;
-*/
+
 return 0;
 }
--- Pratica_4_Jefte-Gomes-105469/pratica4_exercicio2/pratica.4.2.h
+++ Pratica_4_Jefte-Gomes-105469/pratica4_exercicio2/pratica.4.2.h
@@ -6,7 +6,22 @@ class Aquecedor{
         float temperatura;
     public:
         Aquecedor();
+        float getTemperatura();
+        void aquecer();
+        void esfriar();
+
 };
 Aquecedor::Aquecedor(): temperatura(20) {}
+
+float Aquecedor::getTemperatura (){
+    return temperatura;
+}
+
+void Aquecedor::aquecer(){
+    temperatura+=5;
+}
+void Aquecedor::esfriar(){
+    temperatura-=5;
+}
 
 #endif
