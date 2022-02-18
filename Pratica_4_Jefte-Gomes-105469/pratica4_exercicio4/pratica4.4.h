ec4d2905ad911d69c988a062a2ecc586aa58f04e
jefte gomes <jefte.paula@ufv.br>
2022-02-18 18:22:07 -0300

    comecando o exercicio 4

---
+++ Pratica_4_Jefte-Gomes-105469/pratica4_exercicio4/pratica4.4.h
@@ -0,0 +1,21 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+class SavingsAccount
+{
+private:
+    static float annualInterestRate;
+    float savingsBalance;
+public:
+    SavingsAccount(/* args */);
+    ~SavingsAccount();
+};
+
+SavingsAccount::SavingsAccount(/* args */)
+{
+}
+
+SavingsAccount::~SavingsAccount()
+{
+}
+
+#endif
