aeda6f5bda140523f441b13cd7d95a9a770edd11
jefte gomes <jefte.paula@ufv.br>
2022-02-18 19:11:28 -0300

    4.4 pronto

--- Pratica_4_Jefte-Gomes-105469/pratica4_exercicio4/pratica4.4.cpp
+++ Pratica_4_Jefte-Gomes-105469/pratica4_exercicio4/pratica4.4.cpp
@@ -5,17 +5,19 @@ int main ( void ) {
 // Main . Criando duas contas .
 SavingsAccount saver1 = SavingsAccount (2000) ;
 SavingsAccount saver2 = SavingsAccount (3000) ;
-/*
+
+
+
 // Imprimindo o monthly balance
 std :: cout << saver1 . calculateMonthlyInterest () << std :: endl ;
 std :: cout << saver2 . calculateMonthlyInterest () << std :: endl ;
 
 // Alterando atributo static publico
-SavingsAccount :: annualInterestRate = 3.00;
+SavingsAccount :: modifyInterestRate(4.00);
 
 // Imprimindo o monthly balance . Tem que mudar para as duas classes .
 std :: cout << saver1 . calculateMonthlyInterest () << std :: endl ;
 std :: cout << saver2 . calculateMonthlyInterest () << std :: endl ;
-*/
+
 return 0;
 }
--- Pratica_4_Jefte-Gomes-105469/pratica4_exercicio4/pratica4.4.h
+++ Pratica_4_Jefte-Gomes-105469/pratica4_exercicio4/pratica4.4.h
@@ -1,31 +1,39 @@
 #ifndef ACCOUNT_H
 #define ACCOUNT_H
+
 class SavingsAccount
 {
 private:
     static float annualInterestRate;
-    float savingsBalance;
+    float  savingsBalance;
 public:
     SavingsAccount(int val);
     ~SavingsAccount();
-    float savingsBalance();
-    static modifyInterestRate();
+    float getbalance();
+    float calculateMonthlyInterest();
+    static void modifyInterestRate(float NannualInterestRate){
+        annualInterestRate = NannualInterestRate;
+    }
 };
+
+float SavingsAccount::annualInterestRate = 3;
 
 SavingsAccount::SavingsAccount(int val)
 {
+     savingsBalance = val;
+}
+
+float SavingsAccount::getbalance(){
+    return savingsBalance;
+}
+
+float SavingsAccount::calculateMonthlyInterest(){
+    savingsBalance += (savingsBalance*annualInterestRate)/12;
+    return savingsBalance;
 }
 
 SavingsAccount::~SavingsAccount()
 {
 }
 
-float SavingsAccount::savingsBalance(){
-    savingsBalance = (annualInterestRate*savingsBalance)/12;
-    return savingsBalance;
-}
-
-static SavingsAccount::modifyInterestRate(){
-    std::cout << "teste..." << std::endl;
-}
 #endif
