5d55ddb7c2deb7f34dbbb243a8d2e135eaa5872e
jefte gomes <jefte.paula@ufv.br>
2022-02-22 11:17:59 -0300

    "a" pronto

--- Pratica_5_Jefte-Gomes-105469/pratica5.1/pratica.5.1.cpp
+++ Pratica_5_Jefte-Gomes-105469/pratica5.1/pratica.5.1.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include "pessoa.h"
 #include "estudante.h"
+#include "turma.h"
 
 int main(){
+    /*
     Pessoa pessoa("Julio Reis");
     Estudante estudante("Jane Doe", 20180101);
     std::cout << "A pessoa e: " << pessoa.get_nome() << std::endl;
     std::cout << "O estudante e: " << estudante.get_nome() << std::endl;
+    */
+   
+
     return 0;
 };
---
+++ Pratica_5_Jefte-Gomes-105469/pratica5.1/turma.h
@@ -0,0 +1,36 @@
+#ifndef INF112_TURMA_H
+#define INF112_TURMA_H
+#include <string>
+
+class Turma
+{
+private:
+    std::string _codigo;
+    int _ano;
+public:
+    Turma(std::string codigo, int ano);
+    virtual std::string get_codigo();
+    virtual int get_ano();
+    virtual void set_codigo(std::string codigo);
+    virtual void set_ano(int ano);
+};
+
+Turma::Turma(std::string codigo, int ano) : _codigo(codigo), _ano(ano)
+{
+}
+
+std::string Turma::get_codigo(){
+    return this->_codigo;
+}
+
+int Turma::get_ano(){
+    return this->_ano;
+}
+
+void Turma::set_codigo(std::string codigo){
+    _codigo = codigo;
+}
+void Turma::set_ano(int ano){
+    _ano = ano;
+}
+#endif
