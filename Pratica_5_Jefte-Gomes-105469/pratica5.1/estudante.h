213485e866aa003f1cd89561cc65c7ca57ee089f
jefte gomes <jefte.paula@ufv.br>
2022-02-23 19:01:40 -0300

    teste critico

--- Pratica_5_Jefte-Gomes-105469/pratica5.1/estudante.h
+++ Pratica_5_Jefte-Gomes-105469/pratica5.1/estudante.h
@@ -2,11 +2,13 @@
 #define INF112_ESTUDANTE_H
 #include "pessoa.h"
 #include "turma.h"
+//Turma turma;
+//Turma *ptrturma = &turma;
 
 class Estudante : public Pessoa
 {
 private:
-
+    //Turma turma;
     const int _matricula;
 public:
     Estudante(std::string nome, int matricula);
--- Pratica_5_Jefte-Gomes-105469/pratica5.1/turma.h
+++ Pratica_5_Jefte-Gomes-105469/pratica5.1/turma.h
@@ -8,13 +8,18 @@ private:
     std::string _codigo;
     int _ano;
 public:
+    Turma();
     Turma(std::string codigo, int ano);
     virtual std::string get_codigo();
     virtual int get_ano();
     virtual void set_codigo(std::string codigo);
     virtual void set_ano(int ano);
 };
-
+//criando um construtor sem argumentos
+Turma::Turma(){
+    this->_codigo = "null";
+    this->_ano = 0;
+}
 Turma::Turma(std::string codigo, int ano) : _codigo(codigo), _ano(ano)
 {
 }
