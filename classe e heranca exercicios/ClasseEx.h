690fe49509669fb5de7413d219d7a0a6c917693e
jefte gomes <jefte.paula@ufv.br>
2022-03-02 14:35:47 -0300

    pratica 6

--- classe e heranca exercicios/ClasseEx.h
+++ classe e heranca exercicios/ClasseEx.h
@@ -1,14 +1,16 @@
 #ifndef CLASSEX_H
 #define CLASSEX_H
 #include <string>
-class ClasseEx
+#include "Classeif.h"
+
+class ClasseEx : public Classeif
 {
 private:
     std::string _animal1;
     std::string _animal2;
 public:
     ClasseEx(std::string animal1, std::string animal2);
-    void imprimir();
+    virtual void imprimir();
 };
 
 
---
+++ classe e heranca exercicios/Classeif.h
@@ -0,0 +1,15 @@
+#ifndef CLASSEIF_H
+#define CLASSEIF_H
+
+class Classeif
+{
+private:
+
+public:
+    virtual void imprimir() = 0;
+};
+
+
+
+
+#endif
