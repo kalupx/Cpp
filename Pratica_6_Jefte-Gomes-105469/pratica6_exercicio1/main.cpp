d84b070706525c8b11d916294b2592c5def1c2b8
jefte gomes <jefte.paula@ufv.br>
2022-03-07 15:50:44 -0300

    2d pronto

--- Pratica_6_Jefte-Gomes-105469/pratica6_exercicio1/main.cpp
+++ Pratica_6_Jefte-Gomes-105469/pratica6_exercicio1/main.cpp
@@ -6,8 +6,21 @@
 //#include "forma3d.h"
 int main(){
     
+    // lado , nome , cor
+    Quadrado q (2.0 , " Quadrado ", " Vermelho ") ;
+    // raio , nome , cor
+    Circulo c (3.0 , " Circulo ", " Azul ") ;
+    // lado 1 , lado 2 , lado 3/ base , altura , nome , cor
+    Triangulo t1 (7.0 , 3.0 , 4.0 , 2.0 , " TrianguloEscaleno ", " Amarelo ") ;
+    // lado 1/ lado 2 , lado 3/ base , altura , nome , cor
+    Triangulo t2 (3.0 , 4.0 , 2.0 , " Triangulo Isosceles ", " Rosa ") ;
+    // lado 1/ base , altura , nome , cor
+    Triangulo t3 (7.0 ,2.0 , " Triangulo Equilatero ", "Verde ") ;
+    // raio , nome , cor
     
-    
+    std::cout << " Area : " << q.get_area() << " - Perimetro : " << q.get_perimetro() << " - Nome : " << q.get_nome() << " - Cor: " << q.get_cor() << std::endl ;
 
+    std::cout << " Area : " << c.get_area() << " - Perimetro : " << c.get_perimetro() << " - Nome : " << c.get_nome() << " -  Cor: " << c.get_cor() << std::endl ;
+    //continuar os teste com o pdf e fazer os obj 3d
     return 0;
 }
