34c0775b5b6d2202cad8ed9097b3f75f214c7bbd
jefte gomes <jefte.paula@ufv.br>
2022-03-08 16:24:13 -0300

    comecando as formas 3d

--- Pratica_6_Jefte-Gomes-105469/pratica6_exercicio1/main.cpp
+++ Pratica_6_Jefte-Gomes-105469/pratica6_exercicio1/main.cpp
@@ -21,6 +21,13 @@ int main(){
     std::cout << " Area : " << q.get_area() << " - Perimetro : " << q.get_perimetro() << " - Nome : " << q.get_nome() << " - Cor: " << q.get_cor() << std::endl ;
 
     std::cout << " Area : " << c.get_area() << " - Perimetro : " << c.get_perimetro() << " - Nome : " << c.get_nome() << " -  Cor: " << c.get_cor() << std::endl ;
-    //continuar os teste com o pdf e fazer os obj 3d
+    
+    std::cout << " Area : " << t1.get_area() << " - Perimetro : " << t1.get_perimetro() << " - Nome : " << t1.get_nome() << " - Cor: " << t1.get_cor() << std::endl ;
+
+    std::cout << " Area : " << t2.get_area() << " - Perimetro : " << t2.get_perimetro() << " - Nome : " << t2.get_nome () << " - Cor: " << t2.get_cor() << std::endl ;
+
+    std::cout << " Area : " << t3.get_area() << " - Perimetro : " << t3.get_perimetro() << " - Nome : " << t3.get_nome () << " - Cor: " << t3.get_cor() << std::endl ;
+
+    
     return 0;
 }
